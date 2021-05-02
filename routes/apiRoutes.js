70bfde381dae4c1f0e03766aa866c2a648e121bc
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-05-02 14:48:15 -0500

    adding route files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const router = require('express').Router();
+const { createNewNote, noteDelete } = require('../lib/notes.js');
+
+const notes = require('../db/db.json');
+console.log(notes);
+
+router.get('/notes', (req, res) => {
+   
+    let results = notes
+    console.log(results)
+    res.json(results)
+    
+});
+
+router.post('/notes', (req, res) => {
+    let Note = createNewNote(req.body, notes)
+    console.log(Note)
+    res.json(Note) 
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const result = noteDelete(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(400);
+        
+    }
+})
