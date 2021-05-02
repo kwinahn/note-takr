24860bc513b1fe804d3858016b1932d7dac2fec0
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-05-02 14:56:33 -0500

    adding const to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const router = require('express').Router();
+
+
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+
+
