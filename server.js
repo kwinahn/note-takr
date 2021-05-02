e7cb8002a42cfb7a0f1b87f6c28a964f5e7fe9b8
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-05-02 15:01:37 -0500

    parse code and route for server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
@@ -6,6 +7,20 @@ const router = require('express').Router();
 
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
+
+// Sets an initial port.
+const PORT = process.env.PORT || 3000;
+
+//sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.static("public"));
+
+//require routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 
